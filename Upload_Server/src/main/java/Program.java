ef037ad4c793d7240ee78cf5a290f04668a1ccfb
lemon <1456424322@qq.com>
2021-05-26 21:43:04 +0800

    log4j

--- Upload_Server/src/main/java/Program.java
+++ Upload_Server/src/main/java/Program.java
@@ -2,30 +2,20 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        JSONObject object = new JSONObject();
-        //string
-        object.put("string","string");
-        //int
-        object.put("int",2);
-        //boolean
-        object.put("boolean",true);
-        //array
-        List<Integer> integers = Arrays.asList(1,2,3);
-        object.put("list",integers);
-        //null
-        object.put("null",null);
-        //转换为String
-        String str=object.toJSONString();
-        System.out.println(str);
-        //转换为Json
-        JSONObject test= JSON.parseObject(str);
-        //获取字段
-        System.out.println(test.get("string"));
+
+        System.out.println(new Date());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(new Date());
     }
 }
